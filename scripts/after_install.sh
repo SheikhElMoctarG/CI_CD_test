4418495cade532c62f1ae4089481f45475991db1
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-06-04 20:34:39 +0000

    update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,8 +3,4 @@ echo 'run after_install.sh: ' >> /home/ubuntu/CI_CD_test/deploy.log
 
 echo 'cd /home/ubuntu/CI_CD_test' >> /home/ubuntu/CI_CD_test/deploy.log
 cd /home/ubuntu/CI_CD_test >> /home/ubuntu/CI_CD_test/deploy.log
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash 
-sudo chmod +x ~/.nvm/nvm.sh
-sudo ~/.nvm/nvm.sh
-nvm install --lts
 npm install >> /home/ubuntu/CI_CD_test/deploy.log
