d9dbab323913163a3d1a52beb0b408c191ee1852
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-06-04 20:42:04 +0000

    update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,4 +3,7 @@ echo 'run after_install.sh: ' >> /home/ubuntu/CI_CD_test/deploy.log
 
 echo 'cd /home/ubuntu/CI_CD_test' >> /home/ubuntu/CI_CD_test/deploy.log
 cd /home/ubuntu/CI_CD_test >> /home/ubuntu/CI_CD_test/deploy.log
+whoami >> /home/ubuntu/CI_CD_test/deploy.log
+pwd >> /home/ubuntu/CI_CD_test/deploy.log
+
 npm install >> /home/ubuntu/CI_CD_test/deploy.log
