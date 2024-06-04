6f7ef90a0691f623f0ab78208cf0b91f94d0c73a
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-06-04 21:07:47 +0000

    up

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,7 @@
 
 echo 'run application_start.sh: ' >> /home/ubuntu/CI_CD_test/deploy.log
 # nodejs-app is the same name as stored in pm2 process
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
 
 npm run dev >> /home/ubuntu/CI_CD_test/deploy.log
