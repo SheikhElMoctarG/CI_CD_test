8e6881ef8d6d61f9d200fa3ac1a53e219c279b30
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-06-05 20:01:48 +0000

    update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,7 +4,6 @@ echo 'run after_install.sh: ' >> /home/ubuntu/CI_CD_test/deploy.log
 echo 'cd /home/ubuntu/CI_CD_test' >> /home/ubuntu/CI_CD_test/deploy.log
 cd /home/ubuntu/CI_CD_test >> /home/ubuntu/CI_CD_test/deploy.log
 whoami >> /home/ubuntu/CI_CD_test/deploy.log
-pwd >> /home/ubuntu/CI_CD_test/deploy.log
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,8 @@
 
 echo 'run application_start.sh: ' >> /home/ubuntu/CI_CD_test/deploy.log
 # nodejs-app is the same name as stored in pm2 process
+pwd >> /home/ubuntu/CI_CD_test/deploy.log
+
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
 
